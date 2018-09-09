9a27bba174f5724185776dd71b70b53d14bf8f33
Swathi Prasad <swathi.prasad88@gmail.com>
2018-09-09 22:25:51 +0200

    some minor update

--- src/main/java/com/swathisprasad/springboot/SpringRestController.java
+++ src/main/java/com/swathisprasad/springboot/SpringRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author swathip1
+ * @author swathi
  *
  */
 @RestController
