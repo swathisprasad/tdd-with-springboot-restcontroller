1584db23d95926e243cf95c5c580ed6b7f472920
Swathi Prasad <swathi.prasad88@gmail.com>
2018-09-09 22:25:24 +0200

    some minor update

--- src/main/java/com/swathisprasad/springboot/Application.java
+++ src/main/java/com/swathisprasad/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author swathip1
+ * @author swathi
  *
  */
 @SpringBootApplication
